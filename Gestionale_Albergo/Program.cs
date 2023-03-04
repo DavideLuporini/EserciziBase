fc3a09b7c77b070fc6860c5017c3d20cc8e6176c
davide.luporini <davide.luporini@WINAPTMDO3D7THZ>
2023-03-05 00:52:49 +0100

    Added uml scheme

---
+++ Gestionale_Albergo/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Gestionale_Albergo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

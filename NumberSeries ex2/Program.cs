05b1e015a253a4078e790d556eb43c0dbaa7fa02
davide.luporini <davide.luporini@WINAPTMDO3D7THZ>
2023-03-04 18:28:18 +0100

    second exercise ended

--- NumberSeries ex2/Program.cs
+++ NumberSeries ex2/Program.cs
@@ -4,9 +4,48 @@
 
     internal class Program
     {
+
+        //2.Generare una serie di numeri tra 1 e 40 casuali.
+        //La serie termina quando la somma dei numeri generati è maggiore o uguale a 100 
+        //oppure quando è stato raggiunto il numero di tentativi stabiliti.
+        //Per i numeri casuali cercare e provare a utilizzare la classe Random
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            var list = new List<int>();
+
+
+            Console.WriteLine("con quanti numeri desideri sia generata la lista?");
+            var input = int.Parse(Console.ReadLine());
+            generateNumbers(list , input);
+            displayList(list);
+
+
+        }
+
+        public static void generateNumbers(List<int> list , int n)
+        {
+            int number = 0;
+            Random rnd = new Random();
+            for(int i = 0; i < n; i++)
+            {
+                if(number < 100)
+                {
+                    var num = rnd.Next(10, 40);
+                    number += num;
+                    list.Add(num);
+                }
+            }
+            Console.WriteLine(number);
+        }
+        
+        public static void displayList(List<int> list)
+        {
+            Console.WriteLine("la lista è composta dai seguenti numeri :");
+            foreach(var item in list)
+            {
+                Console.Write( " " + item + " ");
+            }
         }
     }
 }
